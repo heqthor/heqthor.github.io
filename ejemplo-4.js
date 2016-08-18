316cbf85c66a93ccbfdf69e9329f5c7d013905ee
heqthor <hector.131427@gmail.com>
2016-08-18 15:43:58 -0500

    Update ejemplo-4.js

--- ejemplo-4.js
+++ ejemplo-4.js
@@ -4,7 +4,7 @@ var camara= new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(windows.innerHeight*.95,window.innerHeight*.95);
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var forma = new THREE.BoxGeometry(1,1,1);
