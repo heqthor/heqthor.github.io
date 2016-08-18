8cf2022b0f03e06241c2531293922a2fe80482ff
heqthor <hector.131427@gmail.com>
2016-08-18 15:56:19 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -1,7 +1,7 @@
 var escena=new THREE.Scene();
 
 var camara= new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=15;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
