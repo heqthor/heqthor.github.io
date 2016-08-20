ce4bf53fbf38062223ad78b870b237f6045a9cb1
heqthor <hector.131427@gmail.com>
2016-08-20 12:46:56 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -2,16 +2,14 @@ var escena=new THREE.Scene();
 
 var camara= new THREE.PerspectiveCamera();
 camara.position.z=50;
-camara.position.x=5;
-camara.position.y=5;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var forma1 = new THREE.CylinderGeometry(5,5,30,64);
-var material1 = new THREE.MeshLambertMaterial();
-var tronco = new THREE.Mesh(forma1,material1);
+var forma = new THREE.CylinderGeometry(5,5,30,64);
+var material = new THREE.MeshNormalMaterial();
+var tronco = new THREE.Mesh(forma,material);
 
 escena.add(tronco);
 
