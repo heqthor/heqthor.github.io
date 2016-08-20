ac96ec8e25a81f05b079db7ca916d16dcd3375e9
heqthor <hector.131427@gmail.com>
2016-08-20 13:47:46 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -13,7 +13,7 @@ var tronco = new THREE.Mesh(formaTronco,materialTronco);
 tronco.position.y=10;
 
 var formaHoja = new THREE.SphereGeometry(10,64,30);
-var materialHoja = new THREE.MeshBasicMaterial({color: 0x35D02A});
+var materialHoja = new THREE.MessLambertMaterial({color: 0x35D02A});
 var hoja = new THREE.Mesh(formaHoja,materialHoja);
 hoja.position.y=20;
 
