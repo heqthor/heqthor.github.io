10e56656dc2db191c6b1ef9959dc83ba1782ac88
heqthor <hector.131427@gmail.com>
2016-08-20 13:44:11 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -7,10 +7,10 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var formaTronco = new THREE.CylinderGeometry(5,5,10,64);
+var formaTronco = new THREE.CylinderGeometry(5,5,20,64);
 var materialTronco = new THREE.MeshBasicMaterial({color: 0x907736});
 var tronco = new THREE.Mesh(formaTronco,materialTronco);
-tronco.position.y=5;
+tronco.position.y=10;
 
 var formaHoja = new THREE.SphereGeometry(10,64,30);
 var materialHoja = new THREE.MeshBasicMaterial({color: 0x35D02A});
