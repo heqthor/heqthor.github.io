67015c1f131c6b3c6237c0e6423b412f0c05f412
heqthor <hector.131427@gmail.com>
2016-08-23 15:37:26 -0500

    Update ejemplo_mallas.js

--- ejemplo_mallas.js
+++ ejemplo_mallas.js
@@ -29,5 +29,5 @@ camara.position.z = 5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight * .95,window.innerHeight * .95);
 
-document.body(appendChild ( renderizador.domElement );
+document.body(appendChild ( renderizador.domElement ); 
 renderizador.render( escena, camara );
