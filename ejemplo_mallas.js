8b22bf38914c72d2f4ae69e300df2415705350c7
heqthor <hector.131427@gmail.com>
2016-08-23 15:45:03 -0500

    Update ejemplo_mallas.js

--- ejemplo_mallas.js
+++ ejemplo_mallas.js
@@ -24,10 +24,10 @@ var escena = new THREE.Scene();
 escena.add( malla );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight * .95,window.innerHeight * .95);
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 
-document.body(appendChild ( renderizador.domElement ); 
+document.body.appendChild ( renderizador.domElement ); 
 renderizador.render( escena, camara );
