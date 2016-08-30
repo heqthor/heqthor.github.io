98efb8daa3f32ef64d335af9477853606166d9c6
heqthor <hector.131427@gmail.com>
2016-08-30 15:28:04 -0500

    Update ejercicio_torre.js

--- rv/ejercicio_torre.js
+++ rv/ejercicio_torre.js
@@ -37,7 +37,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=100;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
