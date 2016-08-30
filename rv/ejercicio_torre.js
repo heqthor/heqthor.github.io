7548ca1322916f6bd84199a22cad30cde4d1b7ee
heqthor <hector.131427@gmail.com>
2016-08-30 15:33:34 -0500

    Update ejercicio_torre.js

--- rv/ejercicio_torre.js
+++ rv/ejercicio_torre.js
@@ -1,20 +1,20 @@
 var puntos=[];
 
-puntos.push(0,0);
-puntos.push(20,0);
-puntos.push(20,10);
-puntos.push(15,10);
-puntos.push(15,15);
-puntos.push(10,15);
-puntos.push(10,40);
-puntos.push(20,40);
-puntos.push(20,45);
-puntos.push(10,45);
-puntos.push(10,50);
-puntos.push(15,50);
-puntos.push(15,60);
-puntos.push(0,60);
-puntos.push(0,0);
+puntos.push(new THREE.Vector2(0,0));
+puntos.push(new THREE.Vector2(20,0));
+puntos.push(new THREE.Vector2(20,10));
+puntos.push(new THREE.Vector2(15,10));
+puntos.push(new THREE.Vector2(15,15));
+puntos.push(new THREE.Vector2(10,15));
+puntos.push(new THREE.Vector2(10,40));
+puntos.push(new THREE.Vector2(20,40));
+puntos.push(new THREE.Vector2(20,45));
+puntos.push(new THREE.Vector2(10,45));
+puntos.push(new THREE.Vector2(10,50));
+puntos.push(new THREE.Vector2(15,50));
+puntos.push(new THREE.Vector2(15,60));
+puntos.push(new THREE.Vector2(0,60));
+puntos.push(new THREE.Vector2(0,0));
 
 var torreForma= new THREE.LatheGeometry(puntos);
 
