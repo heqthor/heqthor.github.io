27ea47c67b999a9be501aaef9975978bd755dd39
heqthor <hector.131427@gmail.com>
2016-09-08 15:29:17 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,11 +1,11 @@
 var tableroForma = new THREE.Geometry();
 var color=0;
+var cuboForma = new THREE.BoxGeometry(10,10,5);
 
 var escena = new THREE.Scene();
 
 for(var i=0;i<=8;i++){
   for(var j=0;j<=8;j++){
-    var cuboForma = new THREE.BoxGeometry(10,10,5);
     cuboForma.translate(-35+i*10,35-j*10,0);
     if(color%2==0){
     var materialCubo = new THREE.MeshBasicMaterial({color: 0x888888});
@@ -17,30 +17,6 @@ for(var i=0;i<=8;i++){
     color=color+1;
   }
 }
-
-var material = new THREE.Mesh();
-
-var tableroMalla = new THREE.Mesh(tableroForma);
-
-/*
-var torreForma= new THREE.Geometry();
-
-torreForma.merge(baseMalla.geometry, baseMalla.matrix);
-
-for(var i=0; i<=6; i++){
-var picoForma = new THREE.CylinderGeometry( 4, 2, 15, 32 );
-  picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),60,13*(Math.cos(Math.PI*2/6*i)));
-var picoMalla =new THREE.Mesh(picoForma); 
-  torreForma.merge(picoMalla.geometry, picoMalla.matrix);
-}
-
-
-var material = new THREE.MeshNormalMaterial();
-
-var torreMalla = new THREE.Mesh(torreForma,material);
-
-*/
-
 
 
 var camara = new THREE.PerspectiveCamera();
