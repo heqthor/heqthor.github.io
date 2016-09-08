9e123a122b17b5f1e9a43d780e1a41fe465dce1a
heqthor <hector.131427@gmail.com>
2016-09-08 14:54:31 -0500

    Create tablero.js

---
+++ rv/tablero.js
@@ -0,0 +1,44 @@
+var tableroForma = new THREE.Geometry();
+
+for(var i=0;i<=8;i++){
+  for(var j=0;j<=8;j++){
+    var cubo = new THREE.BoxGeometry(10,10,5);
+    cubo.translate(-35+i*10,35-j*10,0);
+    var cuboMalla = new THREE.MeshBasicMaterial("#888888");
+    tableroForma.merge(cuboMalla.geometry,cuboMalla.matrix);
+  }
+}
+
+var material = new THREE.MeshNormalMaterial();
+
+var tableroMalla = new THREE.Mesh(tableroForma,material);
+
+/*
+var torreForma= new THREE.Geometry();
+
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+for(var i=0; i<=6; i++){
+var picoForma = new THREE.CylinderGeometry( 4, 2, 15, 32 );
+  picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),60,13*(Math.cos(Math.PI*2/6*i)));
+var picoMalla =new THREE.Mesh(picoForma); 
+  torreForma.merge(picoMalla.geometry, picoMalla.matrix);
+}
+
+
+var material = new THREE.MeshNormalMaterial();
+
+var torreMalla = new THREE.Mesh(torreForma,material);
+
+*/
+
+var escena = new THREE.Scene();
+escena.add(tableroMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=300;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
