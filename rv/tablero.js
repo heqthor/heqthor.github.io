6bc9006d30ab9c8a419b5d22ca0d6ada08af1a05
heqthor <hector.131427@gmail.com>
2016-09-13 15:48:47 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,28 +1,31 @@
 var tableroForma = new THREE.Geometry();
 var color=0;
 var escena = new THREE.Scene();
+var [][] cuboForma;
+var [][] materialCubo;
+var [][] cuboMalla;
 
 for(var i=0;i<=7;i++){
   for(var j=0;j<=7;j++){
-    var cuboForma = new THREE.BoxGeometry(10,10,5);
-    cuboForma.translate(-35+i*10,35-j*10,0);
+    cuboForma[i][j] = new THREE.BoxGeometry(10,10,5);
+    cuboForma[i][j].translate(-35+i*10,35-j*10,0);
     if(color%2==0){
-    var materialCubo = new THREE.MeshBasicMaterial({color: 0x888888});
+    materialCubo[i][j] = new THREE.MeshBasicMaterial({color: 0x888888});
     }else{
-    var materialCubo = new THREE.MeshBasicMaterial({color: 0xffffff});
+    materialCubo[i][j] = new THREE.MeshBasicMaterial({color: 0xffffff});
     }
-    var cuboMalla = new THREE.Mesh(cuboForma,materialCubo);
-    escena.add(cuboMalla);
+    cuboMalla[i][j] = new THREE.Mesh(cuboForma,materialCubo);
     color=color+1;
   }
   color=color+1;
 }
 
+    escena.add(cuboMalla);
 var bordeForma = new THREE.BoxGeometry(100,100,5);
 bordeForma.translate(0,0,-5);
 var bordeMaterial = new THREE.MeshBasicMaterial({color: 0x6b4c1f});
 var bordeMalla = new THREE.Mesh(bordeForma,bordeMaterial);
-/*
+
 escena.add(bordeMalla);
 
 
@@ -35,4 +38,4 @@ camara.lookAt(new THREE.Vector3(0,0,0));
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
-renderizador.render(escena,camara);*/
+renderizador.render(escena,camara);
