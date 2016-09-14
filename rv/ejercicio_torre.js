98647544973de07183a86c00c9bdae06b953ab39
heqthor <hector.131427@gmail.com>
2016-09-13 22:25:05 -0500

    Update ejercicio_torre.js

--- rv/ejercicio_torre.js
+++ rv/ejercicio_torre.js
@@ -46,4 +46,4 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
-/*
+*/
