c21cd74743a03b32662dce9d2fdb273ca69d4ac0
heqthor <hector.131427@gmail.com>
2016-09-22 15:02:47 -0500

    Update ejemploFuncion.js

--- ejemploFuncion.js
+++ ejemploFuncion.js
@@ -1,4 +1,4 @@
-funcion init(p){
+function init(p){
   var malla(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
   escena = new THREE.Scene();
   escena.add(malla);
@@ -7,12 +7,15 @@ funcion init(p){
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
   document.body.appendChild ( renderizador.domElement ); 
-  renderizador.render(escena,camara);
 }
 
-/*
+
 var main = funtion(p){
   renderizador.render(escena,camara);
 }
-*/
+
+var escena, camara, renderizador;///----No hay var en éstas porque son variables globales y no estaban declaradas
+
+init(1);
+main();
   
