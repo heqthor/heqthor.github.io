4cdf90d3464437a9f6bce5a10b8a1355ee7722a9
heqthor <hector.131427@gmail.com>
2016-09-22 14:55:53 -0500

    Update ejemploFuncion.js

--- ejemploFuncion.js
+++ ejemploFuncion.js
@@ -1,2 +1,16 @@
-funcion init(){
+funcion init(p){
+  var malla(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
+  escena = new THREE.Scene();
+  escena.add(malla);
+  
+  camara = new THREE.PerspectiveCamera();
+  renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(700,700);
+}
+
+/*
+var main = funtion(p){
+  renderizador.render(escena,camara);
+}
+*/
   
