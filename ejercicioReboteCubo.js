4a36c8a8dfa36d45bc51c0f729ddca9c7b8cfa38
heqthor <hector.131427@gmail.com>
2016-09-22 15:50:22 -0500

    Update ejercicioReboteCubo.js

--- ejercicioReboteCubo.js
+++ ejercicioReboteCubo.js
@@ -22,19 +22,24 @@ var loop = function(){
   malla.rotateY(0.01);
   malla.rotateX(0.01);
   malla.rotateZ(0.01);
-  if(Math.abs(malla.position.x)>=2){
+  if(Math.abs(malla.position.x)>=1){
     stepX=-stepX;
   }
   malla.position.x+=stepX;
   
-  if(Math.abs(malla.position.y)>=2){
+  if(Math.abs(malla.position.y)>=1){
     stepY=-stepY;
   }
   malla.position.y+=stepY;
   
+  if(Math.abs(malla.position.z)>=1){
+    stepZ=-stepZ;
+  }
+  malla.position.z+=stepZ;
+  
 }
 
-var stepX=0.01,stepY=0.03,malla,escena, camara, renderizador;///----No hay var en éstas porque son variables globales y no estaban declaradas
+var stepX=0.01,stepY=0.03,stepZ=0.09,malla,escena, camara, renderizador;///----No hay var en éstas porque son variables globales y no estaban declaradas
 
 init(1);
 //main();
