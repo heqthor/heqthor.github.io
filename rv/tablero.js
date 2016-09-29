79ebc4e0831837969f820fc32968ae495e7899c3
heqthor <hector.131427@gmail.com>
2016-09-29 14:55:39 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,13 +1,14 @@
-var tableroForma = new THREE.Geometry();
+
 var color=0;
 // var escena = new THREE.Scene();
+var cargador = new THREE.TextureLoader();
 
 for(var i=0;i<8;i++){
   for(var j=0;j<8;j++){
     var cuboForma=  new THREE.BoxGeometry(10,10,5);
     cuboForma.translate(-35+i*10,35-j*10,0);
     if(color%2==0){
-    var materialCubo = new THREE.MeshLambertMaterial({color: 0x888888});
+    var materialCubo = new THREE.MeshLambertMaterial({color: cargador.loader("texturaMarmolNegro.jpg"));
     }else{
     var materialCubo = new THREE.MeshLambertMaterial({color: 0xffffff});
     }
@@ -19,29 +20,6 @@ for(var i=0;i<8;i++){
   color=color+1;
 }
 
-/*
-var  cuboForma = new Array(8);
-var  materialCubo= new Array(8);
-var  cuboMalla = new Array(8);
-
-for(var i=0;i<8;i++){
-  cuboForma[i]=new Array(8);
-  materialCubo[i]=new Array(8);
-  cuboMalla[i]=new Array(8);
-  for(var j=0;j<8;j++){
-    cuboForma[i][j]=  new THREE.BoxGeometry(10,10,5);
-    cuboForma[i][j].translate(-35+i*10,35-j*10,0);
-    if(color%2==0){
-    materialCubo[i][j] = new THREE.MeshBasicMaterial({color: 0x888888});
-    }else{
-    materialCubo[i][j] = new THREE.MeshBasicMaterial({color: 0xffffff});
-    }
-    cuboMalla[i][j] = new THREE.Mesh(cuboForma,materialCubo);
-    color=color+1;
-  }
-  color=color+1;
-}
-*/
 
 var bordeForma = new THREE.BoxGeometry(100,100,5);
 bordeForma.translate(0,0,-5);
