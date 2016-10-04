d8ad2c232c3dd05b6210da14d836b86c8714bfa2
heqthor <hector.131427@gmail.com>
2016-10-04 15:12:23 -0500

    Update ejemploConstructor.js

--- ejemploConstructor.js
+++ ejemploConstructor.js
@@ -32,9 +32,17 @@ CONSTRUCTOR.setup = fuction(){
   CONSTRUCTOR.renderizador.render.setSize(600,600);
   
   CONSTRUCTOR.escena = new THREE.Scene();
-  CONSTRUCTOR.escena.add(arbol1);
-  CONSTRUCTOR.escena.add(arbol2);
+  CONSTRUCTOR.escena.add(arbol1.malla);
+  CONSTRUCTOR.escena.add(arbol2.malla);
+  
+}
+
+CONSTRUCTOR.loop = function(){
+  requestAnimationFrame( CONSTRUCTOR.loop);
   
   CONSTRUCTOR.renderizador.render(CONSTRUCTOR.escena, CONSTRUCTOR.camara);
 }
+
+CONSTRUCTOR.setup();
+CONSTRUCTOR.loop();
   
