c4e6f726e783acb4e9afe1af943dd7c217b0024e
heqthor <hector.131427@gmail.com>
2016-10-04 15:17:11 -0500

    Update ejemploConstructor.js

--- ejemploConstructor.js
+++ ejemploConstructor.js
@@ -6,7 +6,7 @@ function Arbol(){
   var troncoMalla = new THREE.Mesh(troncoForma);
   var esferaMalla = new THREE.Mesh(esferaForma);
   
-  var arbolForma = new THREE.Mesh(troncoForma);
+  var arbolForma = new THREE.Geometry();
   arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
   arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
   
