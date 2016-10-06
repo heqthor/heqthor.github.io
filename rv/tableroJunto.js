445161de88dae3440248d006d8f69a691e344081
heqthor <hector.131427@gmail.com>
2016-10-05 21:56:41 -0500

    Create tableroJunto.js

---
+++ rv/tableroJunto.js
@@ -0,0 +1,35 @@
+var puntos=[];
+
+puntos.push(new THREE.Vector2(0,0));
+puntos.push(new THREE.Vector2(20,0));
+puntos.push(new THREE.Vector2(20,10));
+puntos.push(new THREE.Vector2(15,10));
+puntos.push(new THREE.Vector2(15,15));
+puntos.push(new THREE.Vector2(10,15));
+puntos.push(new THREE.Vector2(10,40));
+puntos.push(new THREE.Vector2(20,40));
+puntos.push(new THREE.Vector2(20,45));
+puntos.push(new THREE.Vector2(10,45));
+puntos.push(new THREE.Vector2(10,50));
+puntos.push(new THREE.Vector2(15,50));
+puntos.push(new THREE.Vector2(15,60));
+puntos.push(new THREE.Vector2(0,60));
+puntos.push(new THREE.Vector2(0,0));
+
+var baseForma= new THREE.LatheGeometry(puntos);
+var baseMalla = new THREE.Mesh(baseForma);
+
+var torreForma= new THREE.Geometry();
+
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+for(var i=0; i<=6; i++){
+var picoForma = new THREE.CylinderGeometry( 4, 2, 15, 32 );
+  picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),60,13*(Math.cos(Math.PI*2/6*i)));
+var picoMalla =new THREE.Mesh(picoForma); 
+  torreForma.merge(picoMalla.geometry, picoMalla.matrix);
+}
+
+
+var material = new THREE.MeshNormalMaterial();
+var torreMalla = new THREE.Mesh(torreForma,material);
