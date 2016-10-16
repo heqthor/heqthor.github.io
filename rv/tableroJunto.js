e53da684672f772f6f6ff711ad9942da6106df3c
heqthor <hector.131427@gmail.com>
2016-10-16 14:15:31 -0500

    Update tableroJunto.js

--- rv/tableroJunto.js
+++ rv/tableroJunto.js
@@ -78,19 +78,19 @@ CONSTRUCTOR.setup = function(){
     setupDone=true;
     var torre1 = new CONSTRUCTOR.Torre(CONSTRUCTOR.torreBlanca);
     torre1.position.x=30;
-    torre1.position.y=10;
+    torre1.position.y=5;
     torre1.position.z=0;
     torre1.scale.x=0.2;
     torre1.scale.y=0.2;
     torre1.scale.z=0.2;
   
     var torre2 = new CONSTRUCTOR.Torre(CONSTRUCTOR.torreNegra);
-    torre2.scale.x=0.2;
-    torre2.scale.y=0.2;
-    torre2.scale.z=0.2;
     torre2.position.x=-30;
     torre2.position.y=5;
     torre2.position.z=0;
+    torre2.scale.x=0.2;
+    torre2.scale.y=0.2;
+    torre2.scale.z=0.2;
 
     var torre3 = new CONSTRUCTOR.Torre(CONSTRUCTOR.torreNegra);
     torre3.position.x=30;
