9250e3f78680323943f114de9c49db095555aa1a
heqthor <hector.131427@gmail.com>
2016-10-20 15:38:43 -0500

    Update movil.js

--- movil.js
+++ movil.js
@@ -31,6 +31,7 @@ function setup(){
 function loop(){
   requestAnimationFrame(loop);
   //caminar();
+  var angulo=0;
   var inclinacion=0.01;
   if(Math.abs(angulo)<=30)inclinacion=-inclinacion;
   angulo=angulo+inclinacion;
