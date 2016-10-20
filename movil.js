713ce3dd4cb04751d26c5ea2fb136f0b3a34ba57
heqthor <hector.131427@gmail.com>
2016-10-20 14:52:59 -0500

    Update movil.js

--- movil.js
+++ movil.js
@@ -1,1 +1,19 @@
 function Pieza(){
+  THREE.Object3D.call(this);
+  this.piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  this.piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
+  this.add(piernaIzq,piernaDer,cuerpo);
+  this.piernaIzq.position.z=-2;
+  this.piernaIzq.position.y=-2.5;
+  this.piernaDer.position.z=-2;
+  this.piernaDer.position.y=-2.5;
+  cuerpo.position.z=2.5;
+}
+
+Pieza.prototype = new THREE.Object3D;
+
+function setup(){
+  var pieza = new Pieza();
+  
+}
