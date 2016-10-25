c908e79465075d205b8c5d24bc025459a9acfb46
heqthor <hector.131427@gmail.com>
2016-10-25 15:47:13 -0500

    Update agenteEjemplo.js

--- agenteEjemplo.js
+++ agenteEjemplo.js
@@ -94,7 +94,7 @@ function setup(){
   entorno.add( new Pared(10,7,-1) );
   entorno.add( new Pared(10,-7,-1) );
   entorno.add( new Pelota(0.5) );
-  entorno.add(new Pelota(1,1,-2) );
+  entorno.add(new Pelota(1,0,-2) );
   entorno.add(camara);
   
   renderer= new THREE.WebGLRenderer();
