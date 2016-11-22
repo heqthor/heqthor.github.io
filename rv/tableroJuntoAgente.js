32e355f6aaf865318c12904e92e329471d1f799e
heqthor <hector.131427@gmail.com>
2016-11-22 15:19:01 -0600

    Update tableroJuntoAgente.js

--- rv/tableroJuntoAgente.js
+++ rv/tableroJuntoAgente.js
@@ -242,6 +242,7 @@ function loop(){
         xBef=xGoal;
         zBef=zGoal;
     }
+    renderi();
     renderizador.render(escena, camara);
 }
 
@@ -395,6 +396,41 @@ var camara = new THREE.PerspectiveCamera();
 var renderizador = new THREE.WebGLRenderer();
 TexturaSetup();
 loop();
+
+
+//-----------------------------------------------------------------------------------------------------------------RAY
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function onMouseMove( event ) {
+
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;		
+
+}
+
+function renderi() {
+
+	// update the picking ray with the camera and mouse position	
+	raycaster.setFromCamera( mouse, camera );	
+
+	// calculate objects intersecting the picking ray
+	var intersects = raycaster.intersectObjects( scene.children );
+
+	for ( var i = 0; i < intersects.length; i++ ) {
+
+		intersects[ i ].object.material.color.set( 0xff0000 );
+	
+	}
+	
+	renderer.render( scene, camera );
+
+}
+
+window.addEventListener( 'click', onMouseMove, false );
 
 
 
