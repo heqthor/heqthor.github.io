3a7d19594d2d5de3637d2b5ab2e0434cef229bf3
heqthor <hector.131427@gmail.com>
2016-11-24 14:51:43 -0600

    Update tableroJuntoAgente.js

--- rv/tableroJuntoAgente.js
+++ rv/tableroJuntoAgente.js
@@ -428,7 +428,7 @@ function onMouseClick( event ) {
 	var intersects = raycaster.intersectObjects( escena.children,true );
 
 	for ( var i = 0; i < intersects.length; i++ ) {
-		if(intersects[i].parent==Torre)
+		if(intersects[i].object.parent==Torre)
 			intersects[ i ].object.material.color.set( 0xff0000 );
 	
 	}
