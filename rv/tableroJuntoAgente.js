655216a44741117bce0fdb14a9a1b40c501e0e32
heqthor <hector.131427@gmail.com>
2016-11-26 14:09:59 -0600

    Update tableroJuntoAgente.js

--- rv/tableroJuntoAgente.js
+++ rv/tableroJuntoAgente.js
@@ -425,7 +425,7 @@ function onMouseClick( event ) {
 		if(intersects[i].object.parent instanceof Torre){
 			intersects[ i ].object.material.color.set( 0xff0000 );
 			piezaTocada=intersects[i].object;
-			console.log(intersects[i].point.x,intersects[i].point.z,intersects[i].object.point.x);
+			console.log(intersects[i].point.x,intersects[i].point.z,intersects[i].object.position.x);
 		}
 		else{
 			if(movimiento==1){
