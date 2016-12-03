a89c547ae9748babe5d94e87e0f692aa4c18b163
heqthor <hector.131427@gmail.com>
2016-12-03 12:58:05 -0600

    Update proyectoIntento2.js

--- rv/proyectoIntento2.js
+++ rv/proyectoIntento2.js
@@ -146,6 +146,7 @@ var Peon=function(textura){
     peonForma.merge(puntaPeonMalla.geometry, puntaPeonMalla.matrix);
         
     //THREE.Mesh.call(this, peonForma, new THREE.MeshLambertMaterial({map:textura}));
+    this.add(new THREE.Mesh(reyForma, new THREE.MeshLambertMaterial({map:textura})));
     this.castShadow=true;
     this.receiveShadow=true;
     if(textura===TEXTURAS.torreBlanca)
