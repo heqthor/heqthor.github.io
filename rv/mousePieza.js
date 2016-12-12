e57a774597ee47bd8f20a1bbad8daaa1e08c4d68
heqthor <hector.131427@gmail.com>
2016-12-12 14:16:05 -0600

    Update mousePieza.js

--- rv/mousePieza.js
+++ rv/mousePieza.js
@@ -201,7 +201,7 @@ function onMouseClick( event ) {
 	
 
 		}
-	}else if( (intersects[0].object.parent instanceof Planos || intersects[1].object.parent instanceof Planos) && movimiento==1){
+	}else if(  movimiento==1){
 		movimiento=0;			
 		tableX=Redondeo(intersects[0].point.x);
 		tableZ=Redondeo(intersects[0].point.z);
