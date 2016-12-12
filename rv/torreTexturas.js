51d4a48db2f52a504b45bb9715372544fb3c4e39
heqthor <hector.131427@gmail.com>
2016-12-12 13:14:45 -0600

    Create torreTexturas.js

---
+++ rv/torreTexturas.js
@@ -0,0 +1,140 @@
+function Torre(textura){ 
+    var puntos=[];
+    
+    puntos.push(new THREE.Vector2(0,0));
+    puntos.push(new THREE.Vector2(20,0));
+    puntos.push(new THREE.Vector2(20,10));
+    puntos.push(new THREE.Vector2(15,10));
+    puntos.push(new THREE.Vector2(15,15));
+    puntos.push(new THREE.Vector2(10,15));
+    puntos.push(new THREE.Vector2(10,40));
+    puntos.push(new THREE.Vector2(20,40));
+    puntos.push(new THREE.Vector2(20,45));
+    puntos.push(new THREE.Vector2(10,45));
+    puntos.push(new THREE.Vector2(10,50));
+    puntos.push(new THREE.Vector2(15,50));
+    puntos.push(new THREE.Vector2(15,60));
+    puntos.push(new THREE.Vector2(0,60));
+    
+    var baseForma= new THREE.LatheGeometry(puntos);
+    var baseMalla = new THREE.Mesh(baseForma);
+
+    var torreForma= new THREE.Geometry();
+
+    torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+    for(var i=0; i<=6; i++){
+    var picoForma = new THREE.CylinderGeometry( 4, 2, 15, 32 );
+      picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),60,13*(Math.cos(Math.PI*2/6*i)));
+    var picoMalla =new THREE.Mesh(picoForma); 
+      torreForma.merge(picoMalla.geometry, picoMalla.matrix);
+    }
+    var torreHecha=new THREE.Mesh(torreForma, new THREE.MeshLambertMaterial({map: textura}));
+    escena.add(torreHecha);
+    
+}
+
+
+
+
+//------------ TABLERO------
+function Tablero (texturaBlanco, texturaNegro,texturaMadera){
+    var color=0;
+    for(var i=0;i<8;i++){
+      for(var j=0;j<8;j++){
+        var cuboForma=  new THREE.BoxGeometry(10,5,10);
+        cuboForma.translate(-35+i*10,0,35-j*10);
+        if(color%2!==0){
+          var material = new THREE.MeshLambertMaterial({map:texturaNegro});
+        }else{
+          var material = new THREE.MeshLambertMaterial({map: texturaBlanco});
+        }
+        var cuboMalla = new THREE.Mesh(cuboForma,material);
+        color=color+1;
+          cuboMalla.receiveShadow=true;
+        escena.add(cuboMalla);
+      }
+      color=color+1;
+    }
+
+    var bordeForma = new THREE.BoxGeometry(100,100,5);
+    bordeForma.translate(0,0,-5);
+    var bordeMaterial = new THREE.MeshLambertMaterial({map:texturaMadera});
+    var bordeMalla = new THREE.Mesh(bordeForma,bordeMaterial);
+    bordeMalla.rotateX(-Math.PI/2);
+    bordeMalla.receiveShadow=true;
+    escena.add(bordeMalla);
+}
+
+//------------ FUNCION CAMBIO VENTANA-----
+function listener(){
+  camara.aspect = window.innerWidth / window.innerHeight;
+  camara.updateProjectionMatrix();
+  renderizador.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+
+
+	
+var torreN1,reyB;
+var tablero=[];
+var tabCol=[];
+var turno=true;
+//---------- SET UP--------
+function setup(){
+    var tipo_evento = 'resize';
+    var cambioVentana = false;
+
+    window.addEventListener( tipo_evento, listener, cambioVentana);
+    
+    camara.position.x = 100;    
+    camara.position.y = 200;
+    camara.position.z = 100;
+    camara.lookAt(new THREE.Vector3(0,0,0));
+    
+    setupDone=true;
+    
+    var luz=new THREE.PointLight(0xCCCCCC);
+    luz.position.y=50;
+    luz.position.z=100;
+    Torre(TEXTURAS.torreBlanca);
+    
+	Tablero(TEXTURAS.marmolNegro, TEXTURAS.marmolBlanco, TEXTURAS.madera);
+
+	renderizador.setSize( window.innerWidth , window.innerHeight );
+	document.body.appendChild( renderizador.domElement );
+}
+
+var setupDone=false;
+
+function loop(){
+  requestAnimationFrame(loop);
+  if(TEXTURAS.madera!==undefined && TEXTURAS.torreBlanca!==undefined && TEXTURAS.marmolBlanco!==undefined && TEXTURAS.marmolNegro!==undefined && !setupDone){
+      setup();
+  }
+    renderizador.render(escena, camara);
+}
+
+var cargador = new THREE.TextureLoader();
+function TexturaSetup(){
+    cargador.load("texturaMarmolNegro.jpg",
+                  function(textura){ TEXTURAS.torreNegra = textura;});
+    cargador.load("texturaMarmolBlanco.jpg",
+                  function(textura){ TEXTURAS.torreBlanca = textura;});
+    cargador.load("texturaMarmolBlanco.jpg",
+                  function(textura){ TEXTURAS.marmolBlanco = textura;});
+    cargador.load("texturaMarmolNegro.jpg",
+                  function(textura){ TEXTURAS.marmolNegro = textura;});
+    cargador.load("texturaMadera.jpg",
+                  function(textura){ TEXTURAS.madera = textura;});
+    
+}
+
+
+var TEXTURAS= new THREE.Object3D();
+var escena = new Scene();
+var camara = new THREE.PerspectiveCamera();
+var renderizador = new THREE.WebGLRenderer();
+TexturaSetup();
+loop();
